970c21df8741553e8b94e2d751b2ad7085caa772
Ben Gibson <ben.gibson.2011@gmail.com>
2017-08-22 19:42:00 +0100

    dev: Ground work towards 2.0.0

--- src/uk/co/ben_gibson/open/in/git/host/Git/File.java
+++ src/uk/co/ben_gibson/open/in/git/host/Git/File.java
@@ -3,18 +3,26 @@ package uk.co.ben_gibson.open.in.git.host.Git;
 import com.intellij.openapi.vfs.VirtualFile;
 
 /**
- * Represents a version controlled file.
+ * Represents a file under version control.
  */
 public class File
 {
+    private String repositoryPath; // the path relative to the repository.
     private VirtualFile file;
 
-    public File(VirtualFile file)
+    public File(String relativePath, VirtualFile file)
     {
-        this.file = file;
+        this.repositoryPath = relativePath;
+        this.file         = file;
     }
 
-    public String path() {
-        return this.file.getPath();
+    public String repositoryPath()
+    {
+        return this.repositoryPath;
+    }
+
+    public String name()
+    {
+        return this.file.getName();
     }
 }
