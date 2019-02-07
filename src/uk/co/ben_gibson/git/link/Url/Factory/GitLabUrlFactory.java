137e8d9125b8ff37c308e4bf64de72fd89757c64
Claas Augner <github@caugner.de>
2019-02-07 11:43:01 +0100

    Fix multiline selection for GitLab. (#86)

---
+++ src/uk/co/ben_gibson/git/link/Url/Factory/GitLabUrlFactory.java
@@ -0,0 +1,23 @@
+package uk.co.ben_gibson.git.link.Url.Factory;
+
+import uk.co.ben_gibson.git.link.Git.*;
+import uk.co.ben_gibson.git.link.UI.LineSelection;
+
+import java.net.URL;
+
+public class GitLabUrlFactory extends GitHubUrlFactory
+{
+    public boolean supports(RemoteHost host)
+    {
+        return host.isGitLab();
+    }
+
+    private String formatLineSelection(LineSelection lineSelection)
+    {
+        if (lineSelection.isMultiLineSelection()) {
+            return String.format("L%d-%d", lineSelection.start(), lineSelection.end());
+        }
+
+        return String.format("L%d", lineSelection.start());
+    }
+}
