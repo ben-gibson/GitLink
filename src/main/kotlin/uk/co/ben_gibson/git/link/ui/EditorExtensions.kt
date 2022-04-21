e9bf67b4143ce95276290128dc9b89df89d6d4da
Ben Gibson <ben.gibson.2011@gmail.com>
2022-04-21 20:40:49 +0100

    Fix line selection when editor has collapsed lines (#184)

--- src/main/kotlin/uk/co/ben_gibson/git/link/ui/EditorExtensions.kt
+++ src/main/kotlin/uk/co/ben_gibson/git/link/ui/EditorExtensions.kt
@@ -4,10 +4,20 @@ import com.intellij.openapi.editor.Editor
 
 val Editor.lineSelection: LineSelection
     get() {
-        val primaryCaret = caretModel.primaryCaret
+        val caretStates = caretModel.caretsAndSelections
 
-        val start = primaryCaret.selectionStartPosition.line + 1
-        val end = primaryCaret.selectionEndPosition.line + 1
+        if (caretStates.size < 1) {
+            return LineSelection(caretModel.logicalPosition.line + 1)
+        }
 
-        return LineSelection(start, end)
+        val caretState = caretStates[0]
+
+        val start = caretState.selectionStart
+        val end = caretState.selectionEnd
+
+        if (start == null || end == null) {
+            return LineSelection(caretModel.logicalPosition.line + 1)
+        }
+
+        return LineSelection(start.line + 1, end.line + 1)
     }
--- src/main/kotlin/uk/co/ben_gibson/git/link/ui/LineSelection.kt
+++ src/main/kotlin/uk/co/ben_gibson/git/link/ui/LineSelection.kt
@@ -1,3 +1,5 @@
 package uk.co.ben_gibson.git.link.ui
 
-data class LineSelection(val start: Int, val end: Int)
+data class LineSelection(val start: Int, val end: Int) {
+    constructor(start: Int) : this(start, start)
+}
