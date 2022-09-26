18b4daab6a9b780bc764697a103447379d305bf3
Réda Housni Alaoui <reda-alaoui@hey.com>
2022-09-27 00:25:38 +0200

    On pristine cloned projects, platform autodetection is triggered too soon

--- src/main/kotlin/uk/co/ben_gibson/git/link/platform/PlatformDetector.kt
+++ src/main/kotlin/uk/co/ben_gibson/git/link/platform/PlatformDetector.kt
@@ -1,9 +1,13 @@
 package uk.co.ben_gibson.git.link.platform
 
+import com.intellij.dvcs.repo.VcsRepositoryManager
+import com.intellij.dvcs.repo.VcsRepositoryMappingListener
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.vfs.VirtualFile
+import git4idea.repo.GitRepository
 import git4idea.repo.GitRepositoryManager
 import uk.co.ben_gibson.git.link.git.domain
 import uk.co.ben_gibson.git.link.git.locateRemote
@@ -12,20 +16,48 @@ import uk.co.ben_gibson.git.link.settings.ProjectSettings
 
 @Service
 class PlatformDetector(val project: Project) {
-    fun detect() : Platform? {
-        val applicationSettings = service<ApplicationSettings>()
+    fun detect(consumer: (Platform?) -> Unit) {
+        val projectDirectory = project.guessProjectDir()
+        if (projectDirectory == null) {
+           consumer.invoke(null)
+           return
+        }
+
+        getRepositoryForFile(projectDirectory) { repository -> getPlatformForRepository(repository).let(consumer) }
+    }
+
+    private fun getPlatformForRepository(repository: GitRepository?): Platform? {
+        repository ?: return null
+
         val settings = project.service<ProjectSettings>()
 
-        val platforms = project.service<PlatformRepository>()
-
-        val projectDirectory = project.guessProjectDir() ?: return null
-
-        val repository = GitRepositoryManager.getInstance(project).getRepositoryForFile(projectDirectory) ?: return null
-
         val remote = repository.locateRemote(settings.remote) ?: return null
+
+        val applicationSettings = service<ApplicationSettings>()
+        val platforms = project.service<PlatformRepository>()
 
         return remote.domain?.let {
             platforms.getByDomain(it) ?: applicationSettings.findPlatformIdByCustomDomain(it)?.let { id -> platforms.getById(id) }
         }
     }
+
+    private fun getRepositoryForFile(projectDirectory: VirtualFile, consumer: (GitRepository?) -> Unit) {
+        val gitRepositoryManager = GitRepositoryManager.getInstance(project)
+        val repository = gitRepositoryManager.getRepositoryForFile(projectDirectory)
+        if (repository != null) {
+            consumer.invoke(repository)
+            return
+        }
+
+        val busConnection = project.messageBus.connect()
+        busConnection
+            .subscribe(
+                VcsRepositoryManager.VCS_REPOSITORY_MAPPING_UPDATED,
+                VcsRepositoryMappingListener {
+                    busConnection.disconnect()
+                    gitRepositoryManager.getRepositoryForFile(projectDirectory).let(consumer)
+                }
+            )
+    }
+
 }
