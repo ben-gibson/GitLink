bc4e80c602d600f00187517fe584f16f87fe13ac
Regis David Souza Mesquita <github@rdsm.dev>
2023-11-09 12:36:06 +0000

    Adds support to sourcehut

--- src/main/kotlin/uk/co/ben_gibson/git/link/ui/Icons.kt
+++ src/main/kotlin/uk/co/ben_gibson/git/link/ui/Icons.kt
@@ -6,6 +6,7 @@ object Icons {
     val GITLAB = IconLoader.getIcon("/icons/gitlab.svg", javaClass)
     val BITBUCKET = IconLoader.getIcon("/icons/bitbucket.svg", javaClass)
     val GOGS = IconLoader.getIcon("/icons/gogs.svg", javaClass)
+    val SOURCEHUT = IconLoader.getIcon("/icons/sourcehut.svg", javaClass)
     val AZURE = IconLoader.getIcon("/icons/azure.svg", javaClass)
     val GITEA = IconLoader.getIcon("/icons/gitea.svg", javaClass)
     val GIT = IconLoader.getIcon("/icons/git.svg", javaClass)
