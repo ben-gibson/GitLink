c23ab616c85bd5b530717a95a73f64671dbd64f0
Ben Gibson <ben.gibson.2011@gmail.com>
2026-09-02 13:55:03 +0100

    Take the annotation group and select-in names from the bundle
    The select-in target fell back to "Gitlink" while everything else says
    "GitLink".

--- src/main/kotlin/uk/co/ben_gibson/git/link/ui/extensions/SelectInTarget.kt
+++ src/main/kotlin/uk/co/ben_gibson/git/link/ui/extensions/SelectInTarget.kt
@@ -4,6 +4,7 @@ import com.intellij.ide.SelectInContext
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import uk.co.ben_gibson.git.link.Context
+import uk.co.ben_gibson.git.link.GitLinkBundle.message
 import uk.co.ben_gibson.git.link.platform.PlatformLocator
 import uk.co.ben_gibson.git.link.openInBrowser
 import com.intellij.ide.SelectInTarget as IntellijSelectInTarget
@@ -18,6 +19,6 @@ class SelectInTarget(private val project: Project) : IntellijSelectInTarget {
     override fun toString(): String {
         val platform = project.service<PlatformLocator>().locate()
 
-        return platform?.name ?: "Gitlink"
+        return platform?.name ?: message("name")
     }
 }
