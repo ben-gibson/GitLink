6c0cbc8ffb6b65cbf0ebfe8c64f5ae7b9df7c8de
Ben Gibson <ben.gibson.2011@gmail.com>
2026-09-02 13:34:30 +0100

    SQUASH ME

--- src/main/kotlin/uk/co/ben_gibson/git/link/Context.kt
+++ src/main/kotlin/uk/co/ben_gibson/git/link/Context.kt
@@ -4,8 +4,6 @@ import com.intellij.openapi.vfs.VirtualFile
 import uk.co.ben_gibson.git.link.git.Commit as GitCommit
 import uk.co.ben_gibson.git.link.git.LineSelection
 
-// [repositoryFile] is used only to resolve which Git repository the action was triggered from -
-// it isn't necessarily the file being linked to (see Commit.root).
 sealed class Context(val repositoryFile: VirtualFile) {
     data class Commit(
         val root: VirtualFile,
