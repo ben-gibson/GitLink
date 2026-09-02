b33a3491f74670e84e37e4de2eb7e2201e92968e
Ben Gibson <ben.gibson.2011@gmail.com>
2026-09-02 13:56:54 +0100

    Share the URL factory test harness
    Every platform test carried the same parameterised test method and
    imports around its expectations. Turning the expectations into dynamic
    tests keeps one test per case in the report while leaving each file
    holding only its data.

--- src/test/kotlin/uk/co/ben_gibson/git/link/url/GitHubTest.kt
+++ src/test/kotlin/uk/co/ben_gibson/git/link/url/GitHubTest.kt
@@ -1,103 +1,84 @@
 package uk.co.ben_gibson.git.link.url
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-import uk.co.ben_gibson.git.link.platform.GitHub
+import org.junit.jupiter.api.TestFactory
 import uk.co.ben_gibson.git.link.git.File
+import uk.co.ben_gibson.git.link.platform.GitHub
 import uk.co.ben_gibson.git.link.url.UrlTestData.BRANCH_MASTER
 import uk.co.ben_gibson.git.link.url.UrlTestData.BRANCH_WITH_SLASH
 import uk.co.ben_gibson.git.link.url.UrlTestData.BRANCH_WITH_SPACE
 import uk.co.ben_gibson.git.link.url.UrlTestData.COMMIT_FULL
 import uk.co.ben_gibson.git.link.url.UrlTestData.DIR_RESOURCES
 import uk.co.ben_gibson.git.link.url.UrlTestData.DIR_ROOT
+import uk.co.ben_gibson.git.link.url.UrlTestData.Expectation
 import uk.co.ben_gibson.git.link.url.UrlTestData.FILE_JAVA
 import uk.co.ben_gibson.git.link.url.UrlTestData.LINE_SELECTION_RANGE
+import uk.co.ben_gibson.git.link.url.UrlTestData.assertUrls
 import uk.co.ben_gibson.git.link.url.factory.TemplatedUrlFactory
 import uk.co.ben_gibson.url.URL
-import java.util.stream.Stream
 
 class GitHubTest {
+    private val baseUrl = URL.fromString("https://github.com/my/repo")
 
-    companion object {
-        private val BASE_URL = URL.fromString("https://github.com/my/repo")
-
-        @JvmStatic
-        fun urlExpectations(): Stream<Arguments> = Stream.of(
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtBranch(FILE_JAVA, BRANCH_MASTER, LINE_SELECTION_RANGE),
-                "https://github.com/my/repo/blob/master/src/Foo.java#L10-L20",
-                "File at branch with line selection"
+    @TestFactory
+    fun `should generate correct URLs`() = assertUrls(
+        TemplatedUrlFactory(GitHub().templates),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtBranch(FILE_JAVA, BRANCH_MASTER, LINE_SELECTION_RANGE),
+            "https://github.com/my/repo/blob/master/src/Foo.java#L10-L20",
+            "File at branch with line selection"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtBranch(FILE_JAVA, BRANCH_WITH_SLASH, LINE_SELECTION_RANGE),
+            "https://github.com/my/repo/blob/feature/ticket-23/src/Foo.java#L10-L20",
+            "Branch containing a forward slash should not be encoded"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtBranch(FILE_JAVA, BRANCH_WITH_SPACE, LINE_SELECTION_RANGE),
+            "https://github.com/my/repo/blob/feature/ticket%2023/src/Foo.java#L10-L20",
+            "Branch containing a space should be encoded"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtBranch(
+                File("my-image.png", false, "src/foo bar baz/images", false),
+                BRANCH_MASTER,
+                null
             ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtBranch(FILE_JAVA, BRANCH_WITH_SLASH, LINE_SELECTION_RANGE),
-                "https://github.com/my/repo/blob/feature/ticket-23/src/Foo.java#L10-L20",
-                "Branch containing a forward slash should not be encoded"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtBranch(FILE_JAVA, BRANCH_WITH_SPACE, LINE_SELECTION_RANGE),
-                "https://github.com/my/repo/blob/feature/ticket%2023/src/Foo.java#L10-L20",
-                "Branch containing a space should be encoded"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtBranch(
-                    File("my-image.png", false, "src/foo bar baz/images", false),
-                    BRANCH_MASTER,
-                    null
-                ),
-                "https://github.com/my/repo/blob/master/src/foo%20bar%20baz/images/my-image.png",
-                "File with spaces in path should be URL encoded"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtCommit(FILE_JAVA, "main", COMMIT_FULL, LINE_SELECTION_RANGE),
-                "https://github.com/my/repo/blob/b032a0707beac9a2f24b1b7d97ee4f7156de182c/src/Foo.java#L10-L20",
-                "File at specific commit with line selection"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtCommit(DIR_RESOURCES, "main", COMMIT_FULL, null),
-                "https://github.com/my/repo/tree/b032a0707beac9a2f24b1b7d97ee4f7156de182c/src/foo/resources",
-                "Directory at commit should use /tree/ path"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtCommit(DIR_ROOT, "main", COMMIT_FULL, null),
-                "https://github.com/my/repo/tree/b032a0707beac9a2f24b1b7d97ee4f7156de182c",
-                "Repository root should link to tree at commit"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.FileAtCommit(FILE_JAVA, "main", COMMIT_FULL, null),
-                "https://github.com/my/repo/blob/b032a0707beac9a2f24b1b7d97ee4f7156de182c/src/Foo.java",
-                "File at commit without line selection"
-            ),
-            Arguments.of(
-                BASE_URL,
-                UrlOptions.Commit(COMMIT_FULL, "main"),
-                "https://github.com/my/repo/commit/b032a0707beac9a2f24b1b7d97ee4f7156de182c",
-                "Direct commit URL"
-            )
-        )
-    }
-
-    @ParameterizedTest(name = "{3}")
-    @MethodSource("urlExpectations")
-    fun `should generate correct URLs`(baseUrl: URL, options: UrlOptions, expectedUrl: String, description: String) {
-        // Given
-        val factory = TemplatedUrlFactory(GitHub().templates)
-
-        // When
-        val url = factory.createUrl(baseUrl, options)
-
-        // Then
-        assertThat(url.toString())
-            .describedAs(description)
-            .isEqualTo(expectedUrl)
-    }
+            "https://github.com/my/repo/blob/master/src/foo%20bar%20baz/images/my-image.png",
+            "File with spaces in path should be URL encoded"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtCommit(FILE_JAVA, "main", COMMIT_FULL, LINE_SELECTION_RANGE),
+            "https://github.com/my/repo/blob/b032a0707beac9a2f24b1b7d97ee4f7156de182c/src/Foo.java#L10-L20",
+            "File at specific commit with line selection"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtCommit(DIR_RESOURCES, "main", COMMIT_FULL, null),
+            "https://github.com/my/repo/tree/b032a0707beac9a2f24b1b7d97ee4f7156de182c/src/foo/resources",
+            "Directory at commit should use /tree/ path"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtCommit(DIR_ROOT, "main", COMMIT_FULL, null),
+            "https://github.com/my/repo/tree/b032a0707beac9a2f24b1b7d97ee4f7156de182c",
+            "Repository root should link to tree at commit"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.FileAtCommit(FILE_JAVA, "main", COMMIT_FULL, null),
+            "https://github.com/my/repo/blob/b032a0707beac9a2f24b1b7d97ee4f7156de182c/src/Foo.java",
+            "File at commit without line selection"
+        ),
+        Expectation(
+            baseUrl,
+            UrlOptions.Commit(COMMIT_FULL, "main"),
+            "https://github.com/my/repo/commit/b032a0707beac9a2f24b1b7d97ee4f7156de182c",
+            "Direct commit URL"
+        ),
+    )
 }
